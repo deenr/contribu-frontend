908c2c9e2f0bad071d42d2c61c99b7aaead8d8ce
Dean Reymen <deenrayman@outlook.com>
2024-12-30 13:43:32 +0300

    feat: header animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Contribu } from './icons/Contribu';
 import { Button } from './ui/button';
 
 export function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed z-50 left-1/2 translate-x-[-50%] w-1/2 py-4 px-4 sm:px-6 md:px-8 lg:px-12">
-      <div className="bg-background/85 rounded-full backdrop-blur-lg border shadow-sm p-2.5 pl-5 flex flex-row justify-between items-center mx-auto max-w-7xl gap-6 sm:gap-8 md:gap-12">
+    <header
+      className={`
+        fixed z-50 left-1/2 translate-x-[-50%] 
+        px-4 sm:px-6 md:px-8 lg:px-12 
+        transition-all duration-300 ease-in-out
+        ${isSticky ? 'w-full sm:w-3/4 md:w-2/3 xl:w-1/2 py-4' : 'w-full py-2 '}
+      `}
+    >
+      <div
+        className={`
+          p-2.5 pl-5 flex flex-row justify-between items-center mx-auto max-w-7xl gap-6 sm:gap-8 md:gap-12 
+          transition-all duration-300 ease-in-out
+          ${isSticky ? 'bg-background/85 rounded-full backdrop-blur-lg border shadow-sm' : ''}
+        `}
+      >
         <Contribu className="text-xl text-primary h-5 w-fit" />
 
         <div className="flex flex-row gap-3">
