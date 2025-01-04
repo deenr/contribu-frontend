1adb822238597eebfe6ba942ad7b2f96259f15de
Dean Reymen <deenrayman@outlook.com>
2025-01-05 00:17:57 +0300

    chore(ui): form components styling

--- packages/ui/src/components/ui/form.tsx
+++ packages/ui/src/components/ui/form.tsx
@@ -72,7 +72,16 @@ FormLabel.displayName = 'FormLabel';
 const FormControl = React.forwardRef<React.ElementRef<typeof Slot>, React.ComponentPropsWithoutRef<typeof Slot>>(({ ...props }, ref) => {
   const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
 
-  return <Slot ref={ref} id={formItemId} aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`} aria-invalid={!!error} {...props} />;
+  return (
+    <Slot
+      className="aria-[invalid=true]:border-destructive/50"
+      ref={ref}
+      id={formItemId}
+      aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+      aria-invalid={!!error}
+      {...props}
+    />
+  );
 });
 FormControl.displayName = 'FormControl';
 
